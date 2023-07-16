ae1176ccc13611a59a4a08135f23cd3e86e0db27
TheQueenIsDead <30706552+TheQueenIsDead@users.noreply.github.com>
2023-07-16 21:21:09 +1200

    chore: remove guff from event.html and format event as a table

--- models.go
+++ models.go
@@ -11,10 +11,6 @@ type Events struct {
 	Description string
 }
 
-func (e *Events) ToString() string {
-	return fmt.Sprintf("%d, %s, %s", e.Id, e.Time, e.Description)
-}
-
-func (e Events) String() string {
+func (e Events) ToString() string {
 	return fmt.Sprintf("%d, %s, %s", e.Id, e.Time, e.Description)
 }
