8834ac3e70a8a103a87666c5b14d1a73df5ff212
TheQueenIsDead <30706552+TheQueenIsDead@users.noreply.github.com>
2023-07-21 21:32:38 +1200

    chore: remove dead code from custom binding attempts

--- models/event.go
+++ models/event.go
@@ -13,21 +13,22 @@ type EventTimestamp time.Time
 
 type Event struct {
 	gorm.Model
-	//Date             datatypes.Date
 	Date             EventDate `form:"date"`
 	Name             string    `form:"name"`
 	Description      string    `form:"description"`
 	MinimumAttendees int8      `form:"minimumAttendees"`
 }
 
-//type EventBinder struct{}
-
+// EventDate is a custom type that extends the sql ORM library datatype for database operation compatibility,
+// while allowing us to implement unmarshalling functionality as part of the echo web framework struct binding process.
 type EventDate struct {
 	datatypes.Date
 }
 
+// UnmarshalParam decodes and assigns a value from a form or query param.
+// It is invoked as part of the web framework entity binding process, and is triggered by the inclusion of the `form`
+// tag on an EventDate object
 func (ed *EventDate) UnmarshalParam(src string) error {
-	//ts, err := time.Parse(time.RFC3339, src)
 	t, err := time.Parse("2006-01-02T15:04", src)
 	if err != nil {
 		log.WithError(err).WithField("source", src).Error("could not unmarshal eventdate param")
@@ -37,52 +38,3 @@ func (ed *EventDate) UnmarshalParam(src string) error {
 	*ed = EventDate{datatypes.Date(t)}
 	return nil
 }
-
-/*func (eb *EventBinder) BindBody(i interface{}, c echo.Context) error {
-
-	// Intercept form date fields and convert them to golang time.Time values
-	if date := c.FormValue("date"); date != "" {
-		format := "2006-01-02T15:04"
-		ts, err := time.Parse(format, date)
-		if err != nil {
-			return err
-		}
-		c.Request().Form.Set("date", ts.String())
-	}
-
-	// Pass to default binder after converting the timestamp
-	db := new(echo.DefaultBinder)
-	if err := db.BindBody(c, i); err != nil {
-		return err
-	}
-
-	return nil
-}
-func (eb *EventBinder) Bind(i interface{}, c echo.Context) (err error) {
-
-	if c.Request().Header.Get(echo.HeaderContentType) == echo.MIMEApplicationForm {
-		err = eb.BindBody(i, c)
-		return
-	}
-
-	db := new(echo.DefaultBinder)
-	if err := db.Bind(i, c); err != nil {
-		return err
-	}
-	//contentType := c.Request().Header.Get(echo.HeaderContentType)
-	//err = echo.ErrUnsupportedMediaType
-	//if strings.HasPrefix(contentType, echo.MIMEApplicationJSON) {
-	//	if err = json.NewDecoder(c.Request().Body).Decode(i); err != nil {
-	//		return http.ErrBodyNotAllowed
-	//	}
-	//	if tagName := c.Get("ValidateTagName"); tagName == nil {
-	//		c.Set("ValidateTagName", "validate")
-	//	}
-	//	config := &validator.Config{TagName: c.Get("ValidateTagName").(string)}
-	//	validate := validator.New(config)
-	//	if err = validate.Struct(i); err != nil {
-	//		return err
-	//	}
-	//}
-	return err
-}*/
