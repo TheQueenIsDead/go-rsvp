43ed9f88ee09e8db812747ac7cbe5bd15b4831d5
TheQueenIsDead <30706552+TheQueenIsDead@users.noreply.github.com>
2024-02-12 21:02:16 +1300

    feat: add htmx package with consts for response headers

---
+++ pkg/htmx/consts.go
@@ -0,0 +1,17 @@
+package htmx
+
+const (
+
+	// HTMX Response Headers: https://htmx.org/docs/#response-headers
+	HXLocation           = "HX-Location"
+	HXPushURL            = "HX-Push-Url"
+	HXRedirect           = "HX-Redirect"
+	HXRefresh            = "HX-Refresh"
+	HXReplaceUrl         = "HX-Replace-Url"
+	HXReswap             = "HX-Reswap"
+	HXRetarget           = "HX-Retarget"
+	HXReselect           = "HX-Reselect"
+	HXTrigger            = "HX-Trigger"
+	HXTriggerAfterSettle = "HX-Trigger-After-Settle"
+	HXTriggerAfterSwap   = "HX-Trigger-After-Swap"
+)
